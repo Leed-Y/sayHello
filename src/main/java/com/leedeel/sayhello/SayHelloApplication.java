d77fe30b7d0f865034e4770e61f82052d3580d41
leedeel <yueguihai@gmail.com>
2023-04-13 15:49:04 +0800

    say hello

--- src/main/java/com/leedeel/sayhello/SayHelloApplication.java
+++ src/main/java/com/leedeel/sayhello/SayHelloApplication.java
@@ -2,12 +2,22 @@ package com.leedeel.sayhello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SayHelloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SayHelloApplication.class, args);
 	}
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return new String("hello 12345!");
+	}
+
+
 
 }
